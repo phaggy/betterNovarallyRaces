2f1ae499889c97626c2d1cedc8965f3ee3f5c456
phaggy <rynmalikp@gmail.com>
2021-10-11 19:26:20 +0530

    cleanup

--- source/components/displayBalance.tsx
+++ source/components/displayBalance.tsx
@@ -5,41 +5,38 @@ import { balance } from "../util/types";
 const Balances: FC<{
 	snake__balance: balance[];
 }> = ({ snake__balance }) => {
-	console.log({ snake__balance });
-
 	const balances = snake__balance.map((balance, index) => {
-		console.log(balance.balance);
 		if (/SNAKOIL/gi.test(balance.balance))
 			return (
-				<Box marginRight={8}>
+				<Box marginRight={8} key={index}>
 					<Text>Snakeoil: </Text>
 					<Text color="yellowBright">{balance.balance}</Text>
 				</Box>
 			);
 		else if (/BOOST/gi.test(balance.balance))
 			return (
-				<Box marginRight={8}>
+				<Box marginRight={8} key={index}>
 					<Text>Boost: </Text>
 					<Text color="yellowBright">{balance.balance}</Text>
 				</Box>
 			);
 		else if (/SNAKGAS/gi.test(balance.balance))
 			return (
-				<Box marginRight={8}>
+				<Box marginRight={8} key={index}>
 					<Text>Snakegas: </Text>
 					<Text color="yellowBright">{balance.balance}</Text>
 				</Box>
 			);
 		else if (/SNAKVENOM/gi.test(balance.balance))
 			return (
-				<Box marginRight={8}>
+				<Box marginRight={8} key={index}>
 					<Text>Snakevenom: </Text>
 					<Text color="yellowBright">{balance.balance}</Text>
 				</Box>
 			);
 		else
 			return (
-				<Box marginRight={8}>
+				<Box marginRight={8} key={index}>
 					<Text>sus: </Text>
 					<Text color="yellowBright">{balance.balance}</Text>
 				</Box>
