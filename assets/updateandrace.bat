50bbfb46ff90f51aa63134acb70376491fb53095
phaggy <rynmalikp@gmail.com>
2021-10-12 23:35:51 +0530

    pre release les go

--- assets/updateandrace.bat
+++ assets/updateandrace.bat
@@ -1,2 +1,4 @@
 curl -L "https://github.com/phaggy/betterNovarallyRaces/releases/latest/download/race-win.exe" -O
+curl -L "https://github.com/phaggy/betterNovarallyRaces/releases/latest/download/race.bat" -O
+
 Start cmd /k race-win.exe --autorace 
