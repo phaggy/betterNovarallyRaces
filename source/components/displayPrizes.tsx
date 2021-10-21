c6ef658d56436a9c6bf057c9d5c29ab34fd0d6ef
phaggy <rynmalikp@gmail.com>
2021-10-22 00:38:03 +0530

    concise pending prizes

--- source/components/displayPrizes.tsx
+++ source/components/displayPrizes.tsx
@@ -4,63 +4,36 @@ import { Box, Text } from "ink";
 const DisplayPrizes: FC<{
 	pending_prizes: Array<any>;
 }> = ({ pending_prizes }) => {
+	let prizes_template = [
+		{ prize: "Common character shard", id: "255777", count: 0 },
+		{ prize: "Uncommon character shard", id: "255782", count: 0 },
+		{ prize: "Rare character shard", id: "255785", count: 0 },
+		{ prize: "Common car shard", id: "255783", count: 0 },
+		{ prize: "Uncommon car shard", id: "255786", count: 0 },
+		{ prize: "Rare car shard", id: "255787", count: 0 },
+	];
+	pending_prizes.map((prize_id) => {
+		prizes_template = prizes_template.map((prize_obj) => {
+			if (String(prize_id) === prize_obj.id)
+				return { ...prize_obj, count: prize_obj.count + 1 };
+			else return prize_obj;
+		});
+	});
+	console.log(prizes_template);
 	return (
 		<>
 			<Box marginLeft={4} padding={1}>
 				<Text>
 					Pending prizes: [
-					{pending_prizes.map((prize_id, index) => {
-						switch (String(prize_id)) {
-							case "255777":
-								return (
-									<Text color="greenBright" key={index}>
-										{" "}
-										|Common character shard|{" "}
-									</Text>
-								);
-							case "255782":
-								return (
-									<Text color="greenBright" key={index}>
-										{" "}
-										|Uncommon character shard|{" "}
-									</Text>
-								);
-							case "255785":
-								return (
-									<Text color="greenBright" key={index}>
-										{" "}
-										|Rare character shard|{" "}
-									</Text>
-								);
-							case "255783":
-								return (
-									<Text color="greenBright" key={index}>
-										{" "}
-										|Common car shard|{" "}
-									</Text>
-								);
-							case "255787":
-								return (
-									<Text color="greenBright" key={index}>
-										{" "}
-										|Rare car shard|{" "}
-									</Text>
-								);
-							case "255786":
-								return (
-									<Text color="greenBright" key={index}>
-										{" "}
-										|Uncommon car shard|{" "}
-									</Text>
-								);
-							default:
-								return (
-									<Text color="greenBright" key={index}>
-										{" "}
-										|{prize_id}|{" "}
-									</Text>
-								);
-						}
+					{prizes_template.map(({ prize, count }, index) => {
+						if (count > 0)
+							return (
+								<Text color="greenBright" key={index}>
+									{" "}
+									|{prize} x{count}|{" "}
+								</Text>
+							);
+						else return <></>;
 					})}
 					]
 				</Text>
